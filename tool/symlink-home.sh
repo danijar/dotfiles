36a2a7fe495a6cb3370c4b72ce6527bd7dcab4ce
Danijar Hafner <mail@danijar.com>
2015-11-28 14:33:09 +0100

    Add script to create symlinks

---
+++ tool/symlink-home.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+repo=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")
+echo "Found repository at $repo"
+
+# link_dir(repo_dir, system_dir)
+# Crate symlinks in the system dir to all files inside the repository dir.
+link_dir() {
+    find $repo/$1 -type f -print0 | while IFS= read -r -d '' file; do
+        link=$2/${file#$repo/$1/}
+        echo $link
+        mkdir -p "$(dirname "$link")"
+        ln -s "$file" "$link"
+    done;
+}
+
+link_dir home ~
