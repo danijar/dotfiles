334017462923165af6a27667141dba242c9decdc
Danijar Hafner <mail@danijar.com>
2016-02-22 15:46:38 +0100

    Split xorg configuration

---
+++ home/bin/keyboard.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+setxkbmap -layout us -variant altgr-intl -option nodeadkeys -option ctrl:nocaps
+xbindkeys
+xmodmap ~/.Xmodmap
--- home/bin/startup.sh
+++ home/bin/startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Try to start tray applications.
 nm-applet &
@@ -9,46 +9,10 @@ redshift-gtk &
 # compton -b &
 
 # Set wallpaper or color.
-# nitrogen --restore &
 hsetroot -solid #000000
 
 # Try to start Ubuntu specific deamons.
 if [ 'python -mplatform | grep Ubuntu' ]; then
-    echo 'Start Ubuntu deamons'
     unity-settings-daemon &
     /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-fi
-
-# Detect the names of connected screens.
-outputs=($(xrandr | grep ' connected' | cut -d ' ' -f1))
-for i in "${!outputs[@]}"; do
-    echo "Output $i: ${outputs[$i]}"
-done
-
-# Three screens are my desktop layout.
-if [ ${#outputs[@]} = "3" ]; then
-    i3-msg "workspace 1;
-        move workspace to output ${outputs[2]};
-        exec urxvtcd; exec urxvtcd"
-    i3-msg "workspace 2;
-        move workspace to output ${outputs[2]};
-        exec urxvtcd; exec urxvtcd"
-    i3-msg "workspace 4;
-        move workspace to output ${outputs[1]};
-        exec urxvtcd; exec urxvtcd"
-    i3-msg "workspace 5;
-        move workspace to output ${outputs[1]};
-        exec urxvtcd; exec urxvtcd"
-    i3-msg "workspace 3;
-        move workspace to output ${outputs[0]};
-        exec --no-startup-id google-chrome-stable"
-
-# One screen is my laptop layout.
-elif [ ${#outputs[@]} = "1" ]; then
-    i3-msg "workspace 1;
-        exec urxvtcd; exec urxvtcd"
-    i3-msg "workspace 2;
-        exec urxvtcd; exec urxvtcd"
-    i3-msg "workspace 3;
-        exec --no-startup-id google-chrome-stable"
 fi
