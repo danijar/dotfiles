9a82920e0aaea0637fa51624fec5a21fecae10d9
Danijar Hafner <mail@danijar.com>
2020-04-21 23:49:39 -0400

    Add new symlink script.

---
+++ symlink.py
@@ -0,0 +1,52 @@
+import os
+import pathlib
+
+
+def is_child(child, parent):
+  try:
+    child.relative_to(parent)
+    return True
+  except ValueError:
+    return False
+
+
+def remove_broken_links(repo, user):
+  # Only search in directories that are mirrored in the repository and only
+  # report broken links that point to the repository. It's good to be careful
+  # because links in mounted network drives may been broken from the client.
+  for directory in repo.glob('**/*'):
+    if not directory.is_dir():
+      continue
+    for path in (user / directory.relative_to(repo)).glob('*'):
+      target = path.resolve()
+      if is_child(target, repo) and not target.exists():
+        print('Broken:', path)
+        path.unlink()
+
+
+def create_links(repo, user):
+  for path in repo.glob('**/*'):
+    dest = user / path.relative_to(repo)
+    if path.is_dir():
+      dest.mkdir(exist_ok=True)
+      continue
+    if not dest.exists():
+      print('Create:', dest, '->', path)
+      dest.symlink_to(path)
+      continue
+    if dest.is_symlink() and is_child(dest.resolve(), repo):
+      assert dest.resolve() == path, (dest, dest.resolve())
+      continue
+    print('Exists:', dest)
+
+
+def main():
+  repo = (pathlib.Path(__file__).parent / 'home').resolve()
+  user = pathlib.Path('~').expanduser().resolve()
+  remove_broken_links(repo, user)
+  create_links(repo, user)
+  print('Done.')
+
+
+if __name__ == '__main__':
+  main()
