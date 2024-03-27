db0d2f1e203e0caedeed32a2805a3506ef8baf2f
Danijar Hafner <mail@danijar.com>
2024-03-27 01:19:04 +0000

    Speed up ipython startup

--- home/scripts/py_imports.py
+++ home/scripts/py_imports.py
@@ -1,25 +1,18 @@
+import collections
+import functools
 import json
 import pathlib
 import re
-from collections import defaultdict, deque
-from functools import partial as bind
 
 import imageio
 import numpy as np
 import pandas as pd
-from PIL import Image
-from tensorflow_probability import distributions as tfd
 
 random = np.random.default_rng(seed=0)
 
-def load_json(path):
-  return json.loads(pathlib.Path(path).read_text())
-
-def save_json(path, data):
-  return pathlib.Path(path).write_text(json.dumps(dataobj))
-
-def resize_image(image, width, height):
+def resize(image, width, height):
   from PIL import Image
   image = Image.fromarray(image)
   image = image.resize((width, height), Image.NEAREST)
   return np.asarray(image)
+
